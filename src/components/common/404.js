a26400af3464ed3c9de4d62082ca5100cc745b45
Ranjan Singh <ranjan.singh@practo.com>
2019-09-25 23:26:52 +0530

    404 page changes

--- src/components/common/404.js
+++ src/components/common/404.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const PageNotFound = () => {
-    return <div>Page not found</div>;
-}
+  return (
+    <React.Fragment>
+      <div>Page not found</div>
+      <Link to="/explore">Explore Mutual Funds</Link>
+    </React.Fragment>
+  );
+};
 
 export default PageNotFound;
