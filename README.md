d113a65e64496fde4f6a5974296a4a69a84743ab
Ranjan Singh <ranjan.singh@practo.com>
2019-09-26 11:51:33 +0530

    api changes

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
-
 In the project directory, you can run:
+
+### `yarn install`
+To install dependencies
 
 ### `yarn start`
 
--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,9 @@
 
 const ApiEndPoints = {
-  exploreFunds:()=> `https://api.kuvera.in/api/v3/funds.json`,
-  fundDetails:(fundCode)=> `https://api.kuvera.in/api/v3/funds/${fundCode}.json`,
+  exploreFunds:()=> `https://jsonblob.com/5d210e6c-e025-11e9-a2ce-3f096ad369a3`,
+  fundDetails:()=> `https://jsonblob.com/8cde6e94-e025-11e9-a2ce-11355300160f`,
 };
-
+// api can be develope to support dynamic code
 export function fetchFunds() {
   return fetch(ApiEndPoints.exploreFunds(), {
     method: 'GET',
